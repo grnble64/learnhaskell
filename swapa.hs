d7c5b3eec7e2407bed6bcb08d94302bb26de0758
菊間大青 <taisei@bluemac.local>
2018-10-19 00:22:03 +0900

    model for 3.6

--- swapa.hs
+++ swapa.hs
@@ -1,10 +1,7 @@
 main = do cs <- getContents
-          putStr $ swapa cs
+          putStr $ map swapa cs
 
-swapa :: String -> String
-swapa cs = map translate cs
-
-translate :: Char -> Char
-translate 'a' = 'A'
-translate 'A' = 'a'
-translate c = c
+swapa :: Char -> Char
+swapa 'a' = 'A'
+swapa 'A' = 'a'
+swapa c = c
