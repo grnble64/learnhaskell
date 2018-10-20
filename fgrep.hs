9fb10eadb057ab3feef97502404ed7831b63397b
菊間大青 <taisei@bluemac.local>
2018-10-21 01:26:24 +0900

    new fgrep.hs

--- fgrep.hs
+++ fgrep.hs
@@ -5,11 +5,7 @@ main = do args <- getArgs
           cs <- getContents
           putStr $ fgrep (head args) cs
 
-fgrep :: String -> String -> String
-fgrep pattern cs = unlines $ filter match $ lines cs
-  where
-    match :: String -> Bool
-    match line = any prefixp $ tails line
+fgrep pattern = unlines . filter (match pattern) . lines
 
-    prefixp :: String -> Bool
-    prefixp line = pattern `isPrefixOf` line
+match pattern = any (pattern `isPrefixOf`) . tails
+
